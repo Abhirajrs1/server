55d7085caabef2e8a490e0740ecbbae126f3d11e
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-10-10 20:08:56 +0530

    S3 bucket cfg added

--- src/Framework/Server/app.js
+++ src/Framework/Server/app.js
@@ -53,7 +53,8 @@ app.use(cors({
 app.use(passport.initialize())
 app.use(passport.session())
 
-
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }))
  app.use(express.json())
  app.use(express.urlencoded({ extended: true }));
 
